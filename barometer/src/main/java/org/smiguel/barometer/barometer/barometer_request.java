1652e66dd1915aadfb6fceb07cbf747ccedf5b08
sulumann <sebastian@miebach.ws>
2015-04-03 00:52:31 +0200

    More Layout and Text fields to be written

--- barometer/src/main/java/org/smiguel/barometer/barometer/barometer_request.java
+++ barometer/src/main/java/org/smiguel/barometer/barometer/barometer_request.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import ws.miebach.sulumann.barometer.R;
 
@@ -25,6 +26,18 @@ public class barometer_request extends ActionBarActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
+
+        EditText TextBaroData = (EditText)findViewById(R.id.TextBarometer);
+        EditText NumberBaroData = (EditText)findViewById(R.id.NumberBarometer);
+        TextBaroData.setText("Toller Text");
+        NumberBaroData.setText("4242");
+
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -32,8 +45,8 @@ public class barometer_request extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.CloseApp) {
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
