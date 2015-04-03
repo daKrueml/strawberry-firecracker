17f28018943f73ab8a566b3f74ab093c40244291
sulumann <sebastian@miebach.ws>
2015-04-03 02:47:11 +0200

    Starting with the Service

---
+++ barometer/src/main/java/org/smiguel/barometer/BarometerSrv.java
@@ -0,0 +1,16 @@
+package org.smiguel.barometer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BarometerSrv extends Service {
+    public BarometerSrv() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
